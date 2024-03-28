232673f31c8601dee9beb076de4bd890de19e60f
Gustavo Serna <gustavoserna@MacBook-Air-de-Gustavo.local>
2024-03-28 14:31:56 -0600

    ip

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    consoleControllerUrl: "http://localhost:9704/api/console/",
-    appControllerUrl: "http://localhost:9704/api/app/",
-    authControllerUrl: "http://localhost:9704/api/auth/",
-    fileControllerUrl: "http://localhost:9704/api/resources/file/"
+    consoleControllerUrl: "http://192.168.2.50:9704/api/console/",
+    appControllerUrl: "http://192.168.2.50:9704/api/app/",
+    authControllerUrl: "http://192.168.2.50:9704/api/auth/",
+    fileControllerUrl: "http://192.168.2.50:9704/api/resources/file/"
   };
